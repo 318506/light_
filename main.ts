6ec2814203688cbdcc79ffaca1cd0c01be4af2de
318506 <91247011+318506@users.noreply.github.com>
2021-10-14 15:45:21 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,18 @@
+let x = 0
+let y = 0
+function 燈數 (num: number) {
+    if (num < 6) {
+        x = num - 1
+        y = 0
+    } else if (num == 6 || num < 10) {
+        x = 4
+        y = num - 4
+    } else if (0 == 0 || 0 == 0) {
+    	
+    } else {
+    	
+    }
+}
+basic.forever(function () {
+	
+})
