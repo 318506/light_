788166988d1d66d3c50222be27eabbe66e53873e
318506 <91247011+318506@users.noreply.github.com>
2021-10-28 16:46:08 +0800

    可以跑了、下次改一閃一閃

--- main.ts
+++ main.ts
@@ -1,18 +1,43 @@
-let x = 0
-let y = 0
 function 燈數 (num: number) {
     if (num < 6) {
         x = num - 1
         y = 0
-    } else if (num == 6 || num < 10) {
+    } else if (num >= 6 && num < 9) {
         x = 4
-        y = num - 4
-    } else if (0 == 0 || 0 == 0) {
-    	
-    } else {
-    	
+        y = num - 5
+    } else if (num >= 9 && num < 14) {
+        x = 13 - num
+        y = 4
+    } else if (num >= 14 && num < 17) {
+        x = 0
+        y = 17 - num
     }
 }
+function 燈數_2 (num: number, 亮燈: boolean) {
+    if (num < 6) {
+        x = num - 1
+        y = 0
+    } else if (num >= 6 && num < 9) {
+        x = 4
+        y = num - 5
+    } else if (num >= 9 && num < 14) {
+        x = 13 - num
+        y = 4
+    } else if (num >= 14 && num < 17) {
+        x = 0
+        y = 17 - num
+    }
+    if (亮燈) {
+        led.plot(x, x)
+    } else {
+        led.unplot(x, y)
+    }
+}
+let y = 0
+let x = 0
+let a = 0
 basic.forever(function () {
-	
+    a += 1
+    basic.pause(100)
+    燈數(a)
 })
