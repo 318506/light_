a59ae67499989dba883712ba367bd5eee9ca2940
318506 <91247011+318506@users.noreply.github.com>
2021-11-04 16:15:28 +0800

    沒什麼變

--- main.ts
+++ main.ts
@@ -27,6 +27,9 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(200)
     }
 })
+/**
+ * 這個還沒用完
+ */
 function 燈數_3 (num: number) {
     if (num < 6) {
         x = num - 1
@@ -41,18 +44,24 @@ function 燈數_3 (num: number) {
         x = 0
         y = 17 - num
     }
-    if (num > 17) {
-        c = 0
-    }
-    if (num < 6) {
-        x = 0 - 0
-        y = 0
-    } else if (false) {
-    	
-    } else {
-    	
-    }
     led.plot(y, x)
+    if (c == 2 && num < 5) {
+        x = num - 1
+        y = 1
+    } else if (5 <= num && num < 7) {
+        y = num - 3
+        x = 3
+    } else if (7 <= num && num < 9) {
+        x = 9 - num
+        y = 3
+    } else if (9 == num) {
+        x = 1
+        y = 2
+    } else if (10 == num) {
+        x = 2
+        y = 2
+    }
+    led.plot(x, y)
 }
 function 燈數_2 (num: number, 亮燈: boolean) {
     if (num < 6) {
@@ -74,10 +83,19 @@ function 燈數_2 (num: number, 亮燈: boolean) {
         led.unplot(y, x)
     }
 }
+/**
+ * B未完
+ */
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     c = 0
     for (let index = 0; index < 16; index++) {
+        c += 1
+        燈數(c)
+        basic.pause(100)
+    }
+    c = 2
+    for (let index = 0; index < 9; index++) {
         c += 1
         燈數(c)
         basic.pause(100)
