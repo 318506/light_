db00bdebd15f30b4343cd6360c22782e6e602295
318506 <91247011+318506@users.noreply.github.com>
2021-11-04 15:58:42 +0800

    一閃一閃好了，正在用全亮的

--- main.ts
+++ main.ts
@@ -12,8 +12,22 @@ function 燈數 (num: number) {
         x = 0
         y = 17 - num
     }
+    led.plot(x, y)
 }
-function 燈數_2 (num: number, 亮燈: boolean) {
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    b = 0
+    for (let index = 0; index < 17; index++) {
+        b += 1
+        燈數_2(b - 1, false)
+        if (b > 16) {
+            b = 1
+        }
+        燈數_2(b, true)
+        basic.pause(200)
+    }
+})
+function 燈數_3 (num: number) {
     if (num < 6) {
         x = num - 1
         y = 0
@@ -27,17 +41,56 @@ function 燈數_2 (num: number, 亮燈: boolean) {
         x = 0
         y = 17 - num
     }
-    if (亮燈) {
-        led.plot(x, x)
+    if (num > 17) {
+        c = 0
+    }
+    if (num < 6) {
+        x = 0 - 0
+        y = 0
+    } else if (false) {
+    	
     } else {
-        led.unplot(x, y)
+    	
+    }
+    led.plot(y, x)
+}
+function 燈數_2 (num: number, 亮燈: boolean) {
+    if (num < 6) {
+        x = num - 1
+        y = 0
+    } else if (num >= 6 && num < 9) {
+        x = 4
+        y = num - 5
+    } else if (num >= 9 && num < 14) {
+        x = 13 - num
+        y = 4
+    } else if (num >= 14 && num <= 16) {
+        x = 0
+        y = 17 - num
+    }
+    if (亮燈 && true) {
+        led.plot(y, x)
+    } else {
+        led.unplot(y, x)
     }
 }
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    c = 0
+    for (let index = 0; index < 16; index++) {
+        c += 1
+        燈數(c)
+        basic.pause(100)
+    }
+})
+let c = 0
+let b = 0
 let y = 0
 let x = 0
+basic.clearScreen()
 let a = 0
 basic.forever(function () {
     a += 1
-    basic.pause(100)
     燈數(a)
+    basic.pause(100)
 })
