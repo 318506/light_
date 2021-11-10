54b0a5ca40f858c2a5058b9bf62af5a38d7c1233
318506 <91247011+318506@users.noreply.github.com>
2021-11-10 23:01:38 +0800

    繞到中間去掉，一閃一閃繞圈好了，一閃一閃+斜線好了

--- main.ts
+++ main.ts
@@ -26,26 +26,8 @@ input.onButtonPressed(Button.A, function () {
         燈數_2(b, true)
         basic.pause(200)
     }
-    led.unplot(3, 4)
-    basic.pause(200)
-    led.unplot(2, 4)
-    basic.pause(200)
-    led.unplot(1, 4)
-    basic.pause(200)
-    led.unplot(0, 4)
-    basic.pause(200)
-    led.unplot(0, 3)
-    basic.pause(200)
-    led.unplot(0, 2)
-    basic.pause(200)
-    led.unplot(0, 1)
-    basic.pause(200)
-    led.unplot(0, 0)
 })
-/**
- * 這個還沒用完
- */
-function 燈數_3 (num: number) {
+function 燈數_3 (num: number, 亮燈燈: boolean) {
     if (num < 6) {
         x = num - 1
         y = 0
@@ -59,24 +41,11 @@ function 燈數_3 (num: number) {
         x = 0
         y = 17 - num
     }
-    led.plot(y, x)
-    if (c == 2 && num < 5) {
-        x = num - 1
-        y = 1
-    } else if (5 <= num && num < 7) {
-        y = num - 3
-        x = 3
-    } else if (7 <= num && num < 9) {
-        x = 9 - num
-        y = 3
-    } else if (9 == num) {
-        x = 1
-        y = 2
-    } else if (10 == num) {
-        x = 2
-        y = 2
+    if (亮燈燈 && true) {
+        led.plot(y, y)
+    } else {
+        led.unplot(x, y)
     }
-    led.plot(x, y)
 }
 function 燈數_2 (num: number, 亮燈: boolean) {
     if (num < 6) {
@@ -88,33 +57,37 @@ function 燈數_2 (num: number, 亮燈: boolean) {
     } else if (num >= 9 && num < 14) {
         x = 13 - num
         y = 4
-    } else if (num >= 14 && num <= 16) {
+    } else if (num >= 14 && num < 17) {
         x = 0
         y = 17 - num
     }
     if (亮燈 && true) {
-        led.plot(y, x)
+        led.plot(x, y)
     } else {
-        led.unplot(y, x)
+        led.unplot(x, y)
     }
 }
-/**
- * B未完
- */
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     c = 0
-    for (let index = 0; index < 16; index++) {
+    for (let index = 0; index < 17; index++) {
         c += 1
-        燈數(c)
-        basic.pause(100)
+        燈數_3(c - 1, false)
+        if (c > 16) {
+            c = 1
+        }
+        燈數_3(c, true)
+        basic.pause(200)
     }
-    c = 2
-    for (let index = 0; index < 9; index++) {
-        c += 1
-        燈數(c)
-        basic.pause(100)
-    }
+    led.unplot(0, 0)
+    basic.pause(200)
+    led.unplot(1, 1)
+    basic.pause(200)
+    led.unplot(2, 2)
+    basic.pause(200)
+    led.unplot(3, 3)
+    basic.pause(200)
+    led.unplot(4, 4)
 })
 let c = 0
 let b = 0
